3cb2dfe787f67a58d378358facb7663104c14288
julien-detat <detat.julien@gmail.com>
2020-11-22 14:36:04 +0100

    Refonte page + ajout d'articles

--- script.js
+++ script.js
@@ -6,104 +6,96 @@
       "Titre": "Apparition des logos",
       "Date":"Fin des années 1400",
       "Résumé":"L'utilisation des logos est arrivée en premier dans le domaine de l'imprimerie",
-      "Catégorie":"Logo",
-      "Lien":"<a href='https://99designs.fr/blog/histoire-design-genres-mouvements/histoire-design-graphique/#firstlogos'>99designs</a>",
-      "Image":"<img src='https://www.agencewebdigitale.com/wp-content/uploads/2012/07/news_webdesign_bases.jpg'>"
+      "Catégorie":"Design",
+      "Lien":"<a href='https://99designs.fr/blog/histoire-design-genres-mouvements/histoire-design-graphique/#firstlogos' target='_blank'>99designs</a>",
+      "Image":"<img src='https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/05/marks.jpg?auto=format&q=60&fit=max&w=930'>"
     },
     {
       "Titre": "Les polices de caractères",
       "Date":"1450",
       "Résumé":"Les polices d'écriture sont nées en même temps que l'imprimerie à caractères mobiles de Johannes Gutenberg vers 1450, qui permettait ainsi de réaliser des exemplaires imprimés en série d'ouvrages",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href='https://www.actualitte.com/article/monde-edition/la-premiere-police-d-ecriture-du-monde/65943#:~:text=Les%20polices%20d'%C3%A9criture%20sont,imprim%C3%A9s%20en%20s%C3%A9rie%20d'ouvrages.'>ActuaLitté</a>",
-      "Image":"<img src='https://www.uxweb-design.com/wp-content/uploads/2019/10/Web-design-1.jpg'>"   
+      "Catégorie":"Design",
+      "Lien":"<a href='https://www.actualitte.com/article/monde-edition/la-premiere-police-d-ecriture-du-monde/65943#:~:text=Les%20polices%20d'%C3%A9criture%20sont,imprim%C3%A9s%20en%20s%C3%A9rie%20d'ouvrages.' target='_blank'>ActuaLitté</a>",
+      "Image":"<img src='https://c4.staticflickr.com/4/3822/9730999371_400e394b63_z.jpg'>"   
     },
     {
+      "Titre": "La société Pantone",
+      "Date":"1866",
+      "Résumé":"À l'origine, Pantone est une petite société, fondée en 1866, qui fabrique des nuanciers pour les fabricants de cosmétiques. Lawrence Herbert rejoint Pantone en 1956 et la rachète en 1962. Il y développe son premier système de couleurs Pantone Matching System ou PMS pour l'impression en 1963.",
+      "Catégorie":"Couleur",
+      "Lien":"<a href='https://fr.wikipedia.org/wiki/Pantone_(entreprise)#:~:text=%C3%80%20l'origine%2C%20Pantone%20est,pour%20l'impression%20en%201963.' target='_blank'>Wikipédia - Pantone (entreprise)</a>",
+      "Image":"<img src='https://upload.wikimedia.org/wikipedia/fr/thumb/3/3f/Pantone_2006_logo.svg/langfr-560px-Pantone_2006_logo.svg.png'>"   
+    },
+    {
+      "Titre": "Hertz et la photoélectricité",
+      "Date":"1886",
+      "Résumé":"Hertz découvre en 1886 la photoélectricité : une plaque de métal soumise à une lumière émet des électrons. L’ensemble de ces découvertes concourent à la naissance des écrans devant lesquels nous passons de plus en plus de temps chaque jour !",
+      "Catégorie":"Ordinateur",
+      "Lien":"<a href='https://www.cigref.fr/archives/histoire-cigref/blog/histoire-des-premiers-ecrans-aux-ecrans-tactiles/' target='_blank'>Cigref</a>",
+      "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/9/98/Heinrich_Rudolf_Hertz_2.jpg'>"
+    },
+      {
+      "Titre": "Le tube cathodique de Karl Ferdinand Braun",
+      "Date":"1892",
+      "Résumé":"Karl Ferdinand Braun invente le tube cathodique, ou CRT pour Cathode Ray Tube.  Le premier écran à tube cathodique fonctionnait à partir d’une diode à cathode froide. La cathode, qui donne son nom au tube cathodique, est la source de production des électrons, c’est-à-dire une électrode métallique chargée négativement, d’une ou plusieurs anodes.",
+      "Catégorie":"Ordinateur",
+      "Lien":"<a href='https://www.cigref.fr/archives/histoire-cigref/blog/histoire-des-premiers-ecrans-aux-ecrans-tactiles/' target='_blank'>Cigref</a>",
+      "Image":"<img src='https://www.cigref.fr/archives/histoire-cigref/wp-content/uploads/2012/03/tube-cathodique.jpg'>"   
+    },
+      {
+      "Titre": "La Wiener Werkstätte (première agence de design graphique)",
+      "Date":"1903",
+      "Résumé":"Avec de plus en plus d’entreprises louant les bénéfices du design graphique, ce n’était qu’une question de temps avant que la première agence voit le jour. Et l’honneur est revenu à la Wiener Werkstätte autrichienne, une organisation qui a contribué à la naissance du design ainsi que de nombreuses entreprises.",
+      "Catégorie":"Design",
+      "Lien":"<a href='https://99designs.fr/blog/histoire-design-genres-mouvements/histoire-design-graphique/#thewiener' target='_blank'>99designs</a>",
+      "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/WW_logo.svg/langfr-560px-WW_logo.svg.png'>"   
+    },
+      {
+      "Titre": "Apparition du terme 'Design graphique'",
+      "Date":"1922",
+      "Résumé":"Dans son article « New Kind of Printing Calls for New Design » (une nouvelle technique d’impression appelle un nouveau genre de design), publié dans le Boston Evening transcrit le 29 août 1922, le designer William Addison Dwiggins utilise pour la première fois le terme « design graphique » pour décrire son rôle dans la structuration et la gestion des visuels des livres. Jusque là, les graphistes avaient toujours eu beaucoup de mal à expliquer au commun des mortels en quoi consistait leur métier, exactement.",
+      "Catégorie":"Design",
+      "Lien":"<a href='https://99designs.fr/blog/histoire-design-genres-mouvements/histoire-design-graphique/#graphic' target='_blank'>99designs</a>",
+      "Image":"<img src='https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/05/DwigginsPortrait.jpg?auto=format&q=60&fit=max&w=930'>"   
+    },
+      {
       "Titre": "",
       "Date":"",
       "Résumé":"",
       "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
+      "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
-    {
+      {
       "Titre": "",
       "Date":"",
       "Résumé":"",
       "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
+      "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
-    {
+      {
       "Titre": "",
       "Date":"",
       "Résumé":"",
       "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
+      "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
-    {
+      {
       "Titre": "",
       "Date":"",
       "Résumé":"",
       "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
+      "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
-    {
+      {
       "Titre": "",
       "Date":"",
       "Résumé":"",
       "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
-      "Image":"<img src='#'>"   
-    },
-    {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
-      "Image":"<img src='#'>"   
-    },
-    {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
-      "Image":"<img src='#'>"   
-    },
-    {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
-      "Image":"<img src='#'>"   
-    },
-    {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
-      "Image":"<img src='#'>"   
-    },
-    {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
-      "Image":"<img src='#'>"   
-    },
-    {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href=''></a>",
+      "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
   ]
@@ -120,7 +112,7 @@
                 '<strong>Catégorie: </strong>' + date.Catégorie + '<br/>' +
                 '<strong>Lien: </strong>' + date.Lien + '<br/>' +
                 '<strong></strong>' + date.Image + '<br/>' +
-                '<br/><hr>' +
+                '<br/>' +
               '</li>';
       }).join('');
       + '</ul>';
