21f95db88ff9512b3f408401ca860d61f99b1ea8
julien-detat <detat.julien@gmail.com>
2020-11-24 18:17:56 +0100

    changement du css entier
    j'ai tout changé ça m'a saoulé

--- script.js
+++ script.js
@@ -3,82 +3,82 @@
  //Copier ce premier exemple et remplir avec les dates/articles/liens/catégories
 
     {
-      "Titre": "Apparition des logos",
-      "Date":"Fin des années 1400",
-      "Résumé":"L'utilisation des logos est arrivée en premier dans le domaine de l'imprimerie",
-      "Catégorie":"Design",
-      "Lien":"<a href='https://99designs.fr/blog/histoire-design-genres-mouvements/histoire-design-graphique/#firstlogos' target='_blank'>99designs</a>",
+      "Titre": " Apparition des logos",
+      "Date":" Fin des années 1400",
+      "Résumé":" L'utilisation des logos est arrivée en premier dans le domaine de l'imprimerie.",
+      "Catégorie":" Design",
+      "Lien":"<a href='https://99designs.fr/blog/histoire-design-genres-mouvements/histoire-design-graphique/#firstlogos' target='_blank'> 99designs</a>",
       "Image":"<img src='https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/05/marks.jpg?auto=format&q=60&fit=max&w=930'>"
     },
     {
-      "Titre": "Les polices de caractères",
-      "Date":"1450",
-      "Résumé":"Les polices d'écriture sont nées en même temps que l'imprimerie à caractères mobiles de Johannes Gutenberg vers 1450, qui permettait ainsi de réaliser des exemplaires imprimés en série d'ouvrages",
+      "Titre": " Les polices de caractères",
+      "Date":" 1450",
+      "Résumé":" Les polices d'écriture sont nées en même temps que l'imprimerie à caractères mobiles de Johannes Gutenberg vers 1450, qui permettait ainsi de réaliser des exemplaires imprimés en série d'ouvrages.",
       "Catégorie":"Design",
-      "Lien":"<a href='https://www.actualitte.com/article/monde-edition/la-premiere-police-d-ecriture-du-monde/65943#:~:text=Les%20polices%20d'%C3%A9criture%20sont,imprim%C3%A9s%20en%20s%C3%A9rie%20d'ouvrages.' target='_blank'>ActuaLitté</a>",
+      "Lien":"<a href='https://www.actualitte.com/article/monde-edition/la-premiere-police-d-ecriture-du-monde/65943#:~:text=Les%20polices%20d'%C3%A9criture%20sont,imprim%C3%A9s%20en%20s%C3%A9rie%20d'ouvrages.' target='_blank'> ActuaLitté</a>",
       "Image":"<img src='https://c4.staticflickr.com/4/3822/9730999371_400e394b63_z.jpg'>"   
     },
     {
-      "Titre": "La société Pantone",
-      "Date":"1866",
-      "Résumé":"À l'origine, Pantone est une petite société, fondée en 1866, qui fabrique des nuanciers pour les fabricants de cosmétiques. Lawrence Herbert rejoint Pantone en 1956 et la rachète en 1962. Il y développe son premier système de couleurs Pantone Matching System ou PMS pour l'impression en 1963.",
-      "Catégorie":"Couleur",
+      "Titre": " La société Pantone",
+      "Date":" 1866",
+      "Résumé":" À l'origine, Pantone est une petite société, fondée en 1866, qui fabrique des nuanciers pour les fabricants de cosmétiques. Lawrence Herbert rejoint Pantone en 1956 et la rachète en 1962. Il y développe son premier système de couleurs Pantone Matching System ou PMS pour l'impression en 1963.",
+      "Catégorie":" Couleur",
       "Lien":"<a href='https://fr.wikipedia.org/wiki/Pantone_(entreprise)#:~:text=%C3%80%20l'origine%2C%20Pantone%20est,pour%20l'impression%20en%201963.' target='_blank'>Wikipédia - Pantone (entreprise)</a>",
       "Image":"<img src='https://upload.wikimedia.org/wikipedia/fr/thumb/3/3f/Pantone_2006_logo.svg/langfr-560px-Pantone_2006_logo.svg.png'>"   
     },
     {
-      "Titre": "Hertz et la photoélectricité",
-      "Date":"1886",
-      "Résumé":"Hertz découvre en 1886 la photoélectricité : une plaque de métal soumise à une lumière émet des électrons. L’ensemble de ces découvertes concourent à la naissance des écrans devant lesquels nous passons de plus en plus de temps chaque jour !",
-      "Catégorie":"Ordinateur",
+      "Titre": " Hertz et la photoélectricité",
+      "Date":" 1886",
+      "Résumé":" Hertz découvre en 1886 la photoélectricité : une plaque de métal soumise à une lumière émet des électrons. L’ensemble de ces découvertes concourent à la naissance des écrans devant lesquels nous passons de plus en plus de temps chaque jour !",
+      "Catégorie":" Ordinateur",
       "Lien":"<a href='https://www.cigref.fr/archives/histoire-cigref/blog/histoire-des-premiers-ecrans-aux-ecrans-tactiles/' target='_blank'>Cigref</a>",
       "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/9/98/Heinrich_Rudolf_Hertz_2.jpg'>"
     },
       {
-      "Titre": "Le tube cathodique de Karl Ferdinand Braun",
-      "Date":"1892",
-      "Résumé":"Karl Ferdinand Braun invente le tube cathodique, ou CRT pour Cathode Ray Tube.  Le premier écran à tube cathodique fonctionnait à partir d’une diode à cathode froide. La cathode, qui donne son nom au tube cathodique, est la source de production des électrons, c’est-à-dire une électrode métallique chargée négativement, d’une ou plusieurs anodes.",
-      "Catégorie":"Ordinateur",
+      "Titre": " Le tube cathodique de Karl Ferdinand Braun",
+      "Date":" 1892",
+      "Résumé":" Karl Ferdinand Braun invente le tube cathodique, ou CRT pour Cathode Ray Tube.  Le premier écran à tube cathodique fonctionnait à partir d’une diode à cathode froide. La cathode, qui donne son nom au tube cathodique, est la source de production des électrons, c’est-à-dire une électrode métallique chargée négativement, d’une ou plusieurs anodes.",
+      "Catégorie":" Ordinateur",
       "Lien":"<a href='https://www.cigref.fr/archives/histoire-cigref/blog/histoire-des-premiers-ecrans-aux-ecrans-tactiles/' target='_blank'>Cigref</a>",
       "Image":"<img src='https://www.cigref.fr/archives/histoire-cigref/wp-content/uploads/2012/03/tube-cathodique.jpg'>"   
     },
       {
-      "Titre": "La Wiener Werkstätte (première agence de design graphique)",
-      "Date":"1903",
-      "Résumé":"Avec de plus en plus d’entreprises louant les bénéfices du design graphique, ce n’était qu’une question de temps avant que la première agence voit le jour. Et l’honneur est revenu à la Wiener Werkstätte autrichienne, une organisation qui a contribué à la naissance du design ainsi que de nombreuses entreprises.",
-      "Catégorie":"Design",
+      "Titre": " La Wiener Werkstätte (première agence de design graphique)",
+      "Date":" 1903",
+      "Résumé":" Avec de plus en plus d’entreprises louant les bénéfices du design graphique, ce n’était qu’une question de temps avant que la première agence voit le jour. Et l’honneur est revenu à la Wiener Werkstätte autrichienne, une organisation qui a contribué à la naissance du design ainsi que de nombreuses entreprises.",
+      "Catégorie":" Design",
       "Lien":"<a href='https://99designs.fr/blog/histoire-design-genres-mouvements/histoire-design-graphique/#thewiener' target='_blank'>99designs</a>",
       "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/WW_logo.svg/langfr-560px-WW_logo.svg.png'>"   
     },
       {
-      "Titre": "Apparition du terme 'Design graphique'",
-      "Date":"1922",
-      "Résumé":"Dans son article « New Kind of Printing Calls for New Design » (une nouvelle technique d’impression appelle un nouveau genre de design), publié dans le Boston Evening transcrit le 29 août 1922, le designer William Addison Dwiggins utilise pour la première fois le terme « design graphique » pour décrire son rôle dans la structuration et la gestion des visuels des livres. Jusque là, les graphistes avaient toujours eu beaucoup de mal à expliquer au commun des mortels en quoi consistait leur métier, exactement.",
-      "Catégorie":"Design",
+      "Titre": " Apparition du terme 'Design graphique'",
+      "Date":" 1922",
+      "Résumé":" Dans son article « New Kind of Printing Calls for New Design » (une nouvelle technique d’impression appelle un nouveau genre de design), publié dans le Boston Evening transcrit le 29 août 1922, le designer William Addison Dwiggins utilise pour la première fois le terme « design graphique » pour décrire son rôle dans la structuration et la gestion des visuels des livres. Jusque là, les graphistes avaient toujours eu beaucoup de mal à expliquer au commun des mortels en quoi consistait leur métier, exactement.",
+      "Catégorie":" Design",
       "Lien":"<a href='https://99designs.fr/blog/histoire-design-genres-mouvements/histoire-design-graphique/#graphic' target='_blank'>99designs</a>",
       "Image":"<img src='https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/05/DwigginsPortrait.jpg?auto=format&q=60&fit=max&w=930'>"   
     },
       {
-      "Titre": "Apparition des images de synthèse",
-      "Date":"1950",
-      "Résumé":"Les images de synthèse sont apparues au début des années 1950. Les images de synthèse informatiques actuelles reposent sur les mêmes principes de projection sur un plan et sont indissociables de l’histoire de l’informatique.",
-      "Catégorie":"Image de synthèse",
+      "Titre": " Apparition des images de synthèse",
+      "Date":" 1950",
+      "Résumé":" Les images de synthèse sont apparues au début des années 1950. Les images de synthèse informatiques actuelles reposent sur les mêmes principes de projection sur un plan et sont indissociables de l’histoire de l’informatique.",
+      "Catégorie":" Image de synthèse",
       "Lien":"<a href='http://www.ensci.com/file_intranet/mastere_ctc/etude_Muzhou_Huang.pdf' target='_blank'>Ensci</a>",
       "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/a/ad/Utah_teapot.png'>"   
     },
       {
-      "Titre": "Henry Dreyfuss - Designing for the people",
-      "Date":"1955",
-      "Résumé":"Henry Dreyfuss publie son ouvrage “Designing for the people” détaillant sa vision basée à la fois sur le sens commun et une approche scientifique. Ce livre met ainsi en avant la dimension de plaisir d’utilisation des objets pour en maximiser l’usage. Ce postulat se trouve désormais à la base de toute démarche d’UX-Design.",
-      "Catégorie":"Ordinateur",
+      "Titre": " Henry Dreyfuss - Designing for the people",
+      "Date":" 1955",
+      "Résumé":" Henry Dreyfuss publie son ouvrage “Designing for the people” détaillant sa vision basée à la fois sur le sens commun et une approche scientifique. Ce livre met ainsi en avant la dimension de plaisir d’utilisation des objets pour en maximiser l’usage. Ce postulat se trouve désormais à la base de toute démarche d’UX-Design.",
+      "Catégorie":" Ordinateur",
       "Lien":"<a href='https://www.ux-republic.com/ux-design-les-origines/' target='_blank'>UX-Repblic</a>",
       "Image":"<img src='https://i2.wp.com/www.ux-republic.com/wp-content/uploads/2018/09/ux-design-article-1024x683.jpeg?resize=1024%2C683'>"   
     },
       {
-      "Titre": "Les premiers pas de l'ordinateur dans l'animation",
-      "Date":"1960",
-      "Résumé":"La première image de synthèse, créée dans les années 1960 par William Fetter, était un pilote. Mais l'animation par ordinateur repose aussi sur le développement du jeu vidéo, de la réalité virtuelle, et, bien entendu, sur les progrès de l'informatique et l'invention de la micro-informatique. Ceux-ci réduisent les temps de calcul et, grâce à eux, les animateurs - et tout un chacun - ont accès aux ordinateurs, autrefois réservés à l'armée, aux laboratoires ou aux universités. ",
-      "Catégorie":"Image de synthèse",
+      "Titre": " Les premiers pas de l'ordinateur dans l'animation",
+      "Date":" 1960",
+      "Résumé":" La première image de synthèse, créée dans les années 1960 par William Fetter, était un pilote. Mais l'animation par ordinateur repose aussi sur le développement du jeu vidéo, de la réalité virtuelle, et, bien entendu, sur les progrès de l'informatique et l'invention de la micro-informatique. Ceux-ci réduisent les temps de calcul et, grâce à eux, les animateurs - et tout un chacun - ont accès aux ordinateurs, autrefois réservés à l'armée, aux laboratoires ou aux universités. ",
+      "Catégorie":" Image de synthèse",
       "Lien":"<a href='https://www.futura-sciences.com/tech/dossiers/technologie-cinema-animation-techniques-plus-grands-films-2537/page/4/' target='_blank'>Futura-sciences</a>",
       "Image":"<img src='https://cdn.futura-sciences.com/buildsv6/images/mediumoriginal/3/9/4/3947480f6c_128449_fs-toystory.jpg'>"   
     },
@@ -166,20 +166,21 @@
   
   var render = function(data) {
     var app = document.getElementById('app');
-    var datesHTMLString = '<ul>'+
+    var datesHTMLString = '<div id="timeline-content"><ul class="timeline">'+
       data.map(function(date){
         // Retourne les données sour forme de liste
-        return '<li>'+
-                '<strong><h3>Titre: </strong>' + date.Titre + '</h3>' +
-                '<strong><time>Date: </strong>' + date.Date + '</time><br/>' +
-                '<strong>Résumé: </strong>' + date.Résumé + '<br/>' +
-                '<strong>Catégorie: </strong>' + date.Catégorie + '<br/>' +
-                '<strong>Lien: </strong>' + date.Lien + '<br/>' +
-                '<strong></strong>' + date.Image + '<br/>' +
+        return '<li class="event>'+
+                '<h3>Titre:' + date.Titre + '</h3>' +
+                '<p><time class="event">Date:' + date.Date + '</time></p><br/>' +
+                '<p>Résumé:' + date.Résumé + '</p><br/>' +
+                'Catégorie:' + date.Catégorie + '<br/>' +
+                date.Lien + '<br/>' +
+                date.Image + '<br/>' +
                 '<br/>' +
-              '</li>';
+              '</li>' +
+              '<hr>';
       }).join('');
-      + '</ul>';
+      + '</ul></div>';
 
     app.innerHTML = datesHTMLString;
   }
