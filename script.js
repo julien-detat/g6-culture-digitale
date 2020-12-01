de07e2fac791a85263a1f15d3d8866ca4ee478c8
julien-detat <detat.julien@gmail.com>
2020-12-01 10:59:53 +0100

    ajout d'articles

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  //Copier ce premier exemple et remplir avec les dates/articles/liens/catégories
 
     {
-      "Titre": " Apparition des logos",
+      "Titre": "Apparition des logos",
       "Date":" Fin des années 1400",
       "Résumé":" L'utilisation des logos est arrivée en premier dans le domaine de l'imprimerie.",
       "Catégorie":" Design",
@@ -11,15 +11,15 @@
       "Image":"<img src='https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/05/marks.jpg?auto=format&q=60&fit=max&w=930'>"
     },
     {
-      "Titre": " Les polices de caractères",
+      "Titre": "Les polices de caractères",
       "Date":" 1450",
       "Résumé":" Les polices d'écriture sont nées en même temps que l'imprimerie à caractères mobiles de Johannes Gutenberg vers 1450, qui permettait ainsi de réaliser des exemplaires imprimés en série d'ouvrages.",
-      "Catégorie":"Design",
-      "Lien":"<a href='https://www.actualitte.com/article/monde-edition/la-premiere-police-d-ecriture-du-monde/65943#:~:text=Les%20polices%20d'%C3%A9criture%20sont,imprim%C3%A9s%20en%20s%C3%A9rie%20d'ouvrages.' target='_blank'> ActuaLitté</a>",
+      "Catégorie":" Design",
+      "Lien":"<a href='https://www.actualitte.com/article/monde-edition/la-premiere-police-d-ecriture-du-monde/65943#:~:text=Les%20polices%20d'%C3%A9criture%20sont,imprim%C3%A9s%20en%20s%C3%A9rie%20d'ouvrages.' target='_blank'>ActuaLitté</a>",
       "Image":"<img src='https://c4.staticflickr.com/4/3822/9730999371_400e394b63_z.jpg'>"   
     },
     {
-      "Titre": " La société Pantone",
+      "Titre": "La société Pantone",
       "Date":" 1866",
       "Résumé":" À l'origine, Pantone est une petite société, fondée en 1866, qui fabrique des nuanciers pour les fabricants de cosmétiques. Lawrence Herbert rejoint Pantone en 1956 et la rachète en 1962. Il y développe son premier système de couleurs Pantone Matching System ou PMS pour l'impression en 1963.",
       "Catégorie":" Couleur",
@@ -27,7 +27,7 @@
       "Image":"<img src='https://upload.wikimedia.org/wikipedia/fr/thumb/3/3f/Pantone_2006_logo.svg/langfr-560px-Pantone_2006_logo.svg.png'>"   
     },
     {
-      "Titre": " Hertz et la photoélectricité",
+      "Titre": "Hertz et la photoélectricité",
       "Date":" 1886",
       "Résumé":" Hertz découvre en 1886 la photoélectricité : une plaque de métal soumise à une lumière émet des électrons. L’ensemble de ces découvertes concourent à la naissance des écrans devant lesquels nous passons de plus en plus de temps chaque jour !",
       "Catégorie":" Ordinateur",
@@ -35,7 +35,7 @@
       "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/9/98/Heinrich_Rudolf_Hertz_2.jpg'>"
     },
       {
-      "Titre": " Le tube cathodique de Karl Ferdinand Braun",
+      "Titre": "Le tube cathodique de Karl Ferdinand Braun",
       "Date":" 1892",
       "Résumé":" Karl Ferdinand Braun invente le tube cathodique, ou CRT pour Cathode Ray Tube.  Le premier écran à tube cathodique fonctionnait à partir d’une diode à cathode froide. La cathode, qui donne son nom au tube cathodique, est la source de production des électrons, c’est-à-dire une électrode métallique chargée négativement, d’une ou plusieurs anodes.",
       "Catégorie":" Ordinateur",
@@ -43,7 +43,7 @@
       "Image":"<img src='https://www.cigref.fr/archives/histoire-cigref/wp-content/uploads/2012/03/tube-cathodique.jpg'>"   
     },
       {
-      "Titre": " La Wiener Werkstätte (première agence de design graphique)",
+      "Titre": "La Wiener Werkstätte (première agence de design graphique)",
       "Date":" 1903",
       "Résumé":" Avec de plus en plus d’entreprises louant les bénéfices du design graphique, ce n’était qu’une question de temps avant que la première agence voit le jour. Et l’honneur est revenu à la Wiener Werkstätte autrichienne, une organisation qui a contribué à la naissance du design ainsi que de nombreuses entreprises.",
       "Catégorie":" Design",
@@ -51,7 +51,7 @@
       "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/WW_logo.svg/langfr-560px-WW_logo.svg.png'>"   
     },
       {
-      "Titre": " Apparition du terme 'Design graphique'",
+      "Titre": "Apparition du terme 'Design graphique'",
       "Date":" 1922",
       "Résumé":" Dans son article « New Kind of Printing Calls for New Design » (une nouvelle technique d’impression appelle un nouveau genre de design), publié dans le Boston Evening transcrit le 29 août 1922, le designer William Addison Dwiggins utilise pour la première fois le terme « design graphique » pour décrire son rôle dans la structuration et la gestion des visuels des livres. Jusque là, les graphistes avaient toujours eu beaucoup de mal à expliquer au commun des mortels en quoi consistait leur métier, exactement.",
       "Catégorie":" Design",
@@ -59,7 +59,7 @@
       "Image":"<img src='https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/05/DwigginsPortrait.jpg?auto=format&q=60&fit=max&w=930'>"   
     },
       {
-      "Titre": " Apparition des images de synthèse",
+      "Titre": "Apparition des images de synthèse",
       "Date":" 1950",
       "Résumé":" Les images de synthèse sont apparues au début des années 1950. Les images de synthèse informatiques actuelles reposent sur les mêmes principes de projection sur un plan et sont indissociables de l’histoire de l’informatique.",
       "Catégorie":" Image de synthèse",
@@ -67,7 +67,7 @@
       "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/a/ad/Utah_teapot.png'>"   
     },
       {
-      "Titre": " Henry Dreyfuss - Designing for the people",
+      "Titre": "Henry Dreyfuss - Designing for the people",
       "Date":" 1955",
       "Résumé":" Henry Dreyfuss publie son ouvrage “Designing for the people” détaillant sa vision basée à la fois sur le sens commun et une approche scientifique. Ce livre met ainsi en avant la dimension de plaisir d’utilisation des objets pour en maximiser l’usage. Ce postulat se trouve désormais à la base de toute démarche d’UX-Design.",
       "Catégorie":" Ordinateur",
@@ -75,7 +75,7 @@
       "Image":"<img src='https://i2.wp.com/www.ux-republic.com/wp-content/uploads/2018/09/ux-design-article-1024x683.jpeg?resize=1024%2C683'>"   
     },
       {
-      "Titre": " Les premiers pas de l'ordinateur dans l'animation",
+      "Titre": "Les premiers pas de l'ordinateur dans l'animation",
       "Date":" 1960",
       "Résumé":" La première image de synthèse, créée dans les années 1960 par William Fetter, était un pilote. Mais l'animation par ordinateur repose aussi sur le développement du jeu vidéo, de la réalité virtuelle, et, bien entendu, sur les progrès de l'informatique et l'invention de la micro-informatique. Ceux-ci réduisent les temps de calcul et, grâce à eux, les animateurs - et tout un chacun - ont accès aux ordinateurs, autrefois réservés à l'armée, aux laboratoires ou aux universités. ",
       "Catégorie":" Image de synthèse",
@@ -83,10 +83,74 @@
       "Image":"<img src='https://cdn.futura-sciences.com/buildsv6/images/mediumoriginal/3/9/4/3947480f6c_128449_fs-toystory.jpg'>"   
     },
       {
-      "Titre": "",
-      "Date":"",
+      "Titre": "L'hypertexte",
+      "Date":"Année 60",
+      "Résumé":"Le concept hypertexte a été créé par Ted Nelson dans les années 60. C'est cette technologie que retiendra Tim Berners-Lee pour relier entre elles les ressources de l'Internet dans le World Wide Web.",
+      "Catégorie":" Logiciel",
+      "Lien":"<a href='' target='_blank'>Hypertexte</a>",
+      "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Hyperlink-Wikipedia.svg/440px-Hyperlink-Wikipedia.svg.png'>"   
+    },
+      {
+      "Titre": "L'histoire des couleurs de nos écrans",
+      "Date":"Année 60-70",
+      "Résumé":"Les nouvelles couleurs synthétiques étaient élaborées dans des centres de recherche par des ingénieurs et des scientifiques qui travaillaient avec des artistes. Ainsi, les premiers synthétiseurs vidéo permettaient de fabriquer des couleurs entièrement neuves, qu’on n’avait jamais vues.",
+      "Catégorie":" Couleur",
+      "Lien":"<a href='https://www.nouvelobs.com/rue89/rue89-le-grand-entretien/20150524.RUE9147/petite-histoire-des-couleurs-de-nos-ecrans.html' target='_blank'>L'OBS</a>",
+      "Image":"<img src='#'>"   
+    },
+      {
+      "Titre": "Le traitement d'images",
+      "Date":"1960",
+      "Résumé":"Le véritable essor du traitement d'images n'a lieu que dans les années 1960 quand les ordinateurs commencent à être suffisamment puissants pour travailler sur des images.",
+      "Catégorie":" Ordinateur",
+      "Lien":"<a href='https://fr.wikipedia.org/wiki/Traitement_d%27images#Historique' target='_blank'>Traitement d'image</a>",
+      "Image":"<img src='#'>"   
+    },
+      {
+      "Titre": "Les courbes de Bézier",
+      "Date":"1962",
+      "Résumé":"Pierre Bézier invente les courbes paramétriques qui porteront son nom. Il s’en servit pour la conception de pièces automobiles. Elles seront plus tard employées en 3D pour tracer courbes et surfaces.",
+      "Catégorie":" Logiciel",
+      "Lien":"<a href='http://www.ensci.com/file_intranet/mastere_ctc/etude_Muzhou_Huang.pdf' target='_blank'>Ensci</a>",
+      "Image":"<img src='#'>"   
+    },
+      {
+      "Titre": "Ivan Sutherland - Premier logiciel graphique",
+      "Date":"1963",
+      "Résumé":"C'est au MIT qu'Ivan Sutherland élabore le premier logiciel graphique interactif utilisant un stylo optique pour dessiner sur un écran des schémas techniques?",
+      "Catégorie":" Logiciel",
+      "Lien":"<a href='https://www.itone.lu/actualites/interface-graphique-ou-les-debuts-du-tactile' target='_blank'>iT One</a>",
+      "Image":"<img src='#'>"   
+    },
+      {
+      "Titre": "Le SRI International et l'environnement graphique",
+      "Date":"1968",
+      "Résumé":"La Stanford Reasearch Institute (RSI) fait une démonstration d’un environnement graphique avec des fenêtres à manipuler avec une souris. En même temps, il démontre l’utilisation d’un traitement de texte, d’un système hypertexte et d’un logiciel de travail collaboratif en groupe.",
+      "Catégorie":" Logiciel",
+      "Lien":"<a href='https://www.itone.lu/actualites/interface-graphique-ou-les-debuts-du-tactile' target='_blank'>iT One</a>",
+      "Image":"<img src='#'>"   
+    },
+      {
+      "Titre": "Le début des icônes",
+      "Date":"1970",
+      "Résumé":"C’est un pictogramme de petite dimension affichée sur l’écran d’un ordinateur, représentant une action, un objet, un logiciel, un type de fichier, etc. Les icônes ont dans un premier temps servi d'outils pour rendre les interfaces utilisateur graphiques plus simples d'utilisation.",
+      "Catégorie":" Design",
+      "Lien":"<a href='https://fr.wikipedia.org/wiki/Ic%C3%B4ne_(informatique)' target='_blank'>Icône</a>",
+      "Image":"<img src='https://upload.wikimedia.org/wikipedia/commons/b/b5/Tango-example_icons.png'>"   
+    },
+      {
+      "Titre": "L'apparition de l'infographie",
+      "Date":"1970",
+      "Résumé":"Le terme « infographie » désigne les graphismes produits par ordinateur, plus précisément les diagrammes, cartes et schémas employés comme illustration d'articles.",
+      "Catégorie":" Design",
+      "Lien":"<a href='https://fr.wikipedia.org/wiki/Infographie' target='_blank'>Infographie</a>",
+      "Image":"<img src='#'>"   
+    },
+      {
+      "Titre": "L'industrie vidéoludique",
+      "Date":"1970",
       "Résumé":"",
-      "Catégorie":"Font / Police",
+      "Catégorie":" ",
       "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
@@ -94,7 +158,15 @@
       "Titre": "",
       "Date":"",
       "Résumé":"",
-      "Catégorie":"Font / Police",
+      "Catégorie":" ",
+      "Lien":"<a href='' target='_blank'></a>",
+      "Image":"<img src='#'>"   
+    },
+    {
+      "Titre": "",
+      "Date":"",
+      "Résumé":"",
+      "Catégorie":" ",
       "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
@@ -102,7 +174,15 @@
       "Titre": "",
       "Date":"",
       "Résumé":"",
-      "Catégorie":"Font / Police",
+      "Catégorie":" ",
+      "Lien":"<a href='' target='_blank'></a>",
+      "Image":"<img src='#'>"   
+    },
+    {
+      "Titre": "",
+      "Date":"",
+      "Résumé":"",
+      "Catégorie":" ",
       "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
@@ -110,7 +190,15 @@
       "Titre": "",
       "Date":"",
       "Résumé":"",
-      "Catégorie":"Font / Police",
+      "Catégorie":" ",
+      "Lien":"<a href='' target='_blank'></a>",
+      "Image":"<img src='#'>"   
+    },
+    {
+      "Titre": "",
+      "Date":"",
+      "Résumé":"",
+      "Catégorie":" ",
       "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
@@ -118,47 +206,7 @@
       "Titre": "",
       "Date":"",
       "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href='' target='_blank'></a>",
-      "Image":"<img src='#'>"   
-    },
-      {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href='' target='_blank'></a>",
-      "Image":"<img src='#'>"   
-    },
-      {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href='' target='_blank'></a>",
-      "Image":"<img src='#'>"   
-    },
-      {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href='' target='_blank'></a>",
-      "Image":"<img src='#'>"   
-    },
-      {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
-      "Lien":"<a href='' target='_blank'></a>",
-      "Image":"<img src='#'>"   
-    },
-      {
-      "Titre": "",
-      "Date":"",
-      "Résumé":"",
-      "Catégorie":"Font / Police",
+      "Catégorie":" ",
       "Lien":"<a href='' target='_blank'></a>",
       "Image":"<img src='#'>"   
     },
